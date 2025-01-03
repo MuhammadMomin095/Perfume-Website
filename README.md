1df451f3c0953cc3c56b729d66d5210cd2125a72
MuhammadMomin095 <160387789+MuhammadMomin095@users.noreply.github.com>
2025-01-03 08:15:35 +0500

    first commit

--- README.md
+++ README.md
@@ -41,3 +41,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
 "# mm" 
 "# m" 
 "# mm" 
+"# m" 
--- src/app/order-summary/page.tsx
+++ src/app/order-summary/page.tsx
@@ -19,12 +19,12 @@ const OrderSummaryPage = () => {
     <div className="container mx-auto py-10 px-4">
       {orderSuccess ? (
         <div className="text-center bg-red-500 text-white p-6 rounded-lg shadow-md">
-          <h1 className="text-3xl font-bold mb-4">Oops! </h1>
+          <h1 className="text-3xl font-bold mb-4">Oops!</h1>
           <p className="text-lg">Something went wrong with your order. Please try again.</p>
         </div>
       ) : (
         <div className="text-center bg-green-500 text-white p-6 rounded-lg shadow-md">
-          <h1 className="text-3xl font-bold mb-4">Congratulations!</h1>
+          <h1 className="text-3xl font-bold mb-4">Congratulations!Oops!</h1>
           <p className="text-lg">Your product order has been placed successfully. You will receive your order soon!</p>
         </div>
       )}
